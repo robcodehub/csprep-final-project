7330c32c5ff81a25573bfbefafd4caa9308cee4a
Ammar <ammar@ammarhassan.co.uk>
2020-01-20 12:46:05 +0000

    basic calculation

---
+++ calculation.js
@@ -0,0 +1,36 @@
+//prompt for keyword
+let ask = prompt("What would u like to do today?");
+//when prompted invoke that function
+if(ask == 'multiply'){
+  multiply()
+}else if(ask == 'divide'){
+  divide()
+}else if(ask == 'add'){
+  add()
+}else if(ask == 'minus'){
+  minus()
+}
+
+//functions
+function multiply(num1, num2){
+  let val1 = prompt("Enter value to multiply")
+  let val2 = prompt("Enter value to multiply with " + val1)
+  return val1 * val2;
+}
+function divide(num1, num2){
+  let val1 = prompt("Enter value to divide")
+  let val2 = prompt("Enter value to divide with " + val1)
+  return val1 / val2;
+}
+function add(num1, num2){
+  let val1 = prompt("Enter value to add")
+  let val2 = prompt("Enter value to add with " + val1)
+  return val1 + val2;
+}
+function minus(num1, num2){
+  let val1 = prompt("Enter value to minus")
+  let val2 = prompt("Enter value to minus with " + val1)
+  return val1 - val2;
+}
+
+//TODO - Make it DRY
